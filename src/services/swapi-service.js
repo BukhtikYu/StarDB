462987a69977fa9c209345ce98fb6fc8fa79a982
Yuliya <yuliya.bukhtik@gmail.com>
2020-09-22 13:43:47 +0300

    Added comments to the swapi-service : searching by id HTTP error: status code 404, net::ERR_UNKNOWN_URL_SCHEME

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -17,30 +17,30 @@ export default class SwapiService {
       return res.results;
     };
   
-    async getPerson(id) {
+    /*async getPerson(id) {
       const person = await this.getResource(`/people/${id}/`);
       return person.results;
-    };
+    };*/
   
     async getAllPlanets() {
       const res = await this.getResource(`/planets/`);
       return res.results;
     };
     
-    async getPlanet(id) {
+    /*async getPlanet(id) {
       const planet = await this.getResource(`/planets/${id}/`);
       return planet.results;
-    };
+    };*/
     
     async getAllStarships() {
       const res = await this.getResource(`/starships/`);
       return res.results;
     };
     
-    async getStarship(id) {
+    /*async getStarship(id) {
       const starship = this.getResource(`/starships/${id}/`);
       return starship.results;
-    };
+    };*/
   };
   
   
@@ -54,18 +54,25 @@ export default class SwapiService {
   });*/
   
   /*swapi.getPerson(3)
-       .then ((body) => {
-    console.log(body);
+       .then ((person) => {
+    console.log(person);
   });*/
   
   /*swapi.getAllPlanets().then ((body) => {
-    console.log(body);
+    console.log(body[0]);
   });*/
   
   /*swapi.getAllStarships().then ((body) => {
     console.log(body);
   });*/
   
-  /*swapi.getStarship(1).then ((body) => {
-    console.log(body);
+  /*swapi.getStarship(1).then ((starship) => {
+    console.log(starship);
   });*/
+
+  /*swapi.getPlanet(1).then ((planet) => {
+    console.log(planet);
+  })
+
+  //doesn't work by searcing id:
+  HTTP error: status code 404, net::ERR_UNKNOWN_URL_SCHEME
